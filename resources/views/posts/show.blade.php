df5bf25912928493240ee9ffc141f4008d83a028
Ouali Mohamed <mohamed.ouali2010gmail.com>
2023-04-18 16:56:11 +0100

    display image from local or https sources

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (Session::has('success'))
-        <div class="alert alert-success" role="alert">
-            {{ Session::get('success') }}
-        </div>
-    @endif
     <h2>Post details </h2>
     <div class="card mb-3">
-        <img src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}">
+        @if (Str::contains($post->image, 'https'))
+            <img src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}">
+        @else    
+            <img src="{{ asset('storage/' . $post->image) }}" class="card-img-top" alt="{{ $post->title }}">
+        @endif
         <div class="card-body">
             <h5 class="card-title">{{ $post->title }} <span class="badge bg-primary">{{ $post->category->name }}</span></h5>
             <p class="card-text">Author: {{ $post->user->name }}</p>
